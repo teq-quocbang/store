8bfd4ea01315a1e0e1785fa871d5396e7486ee1a
bangthong <bangthong@teqnological.asia>
2023-11-07 15:22:13 +0700

    fix: mod table account

--- migrations/20231107064314_table_account.up.sql
+++ migrations/20231107064314_table_account.up.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS
     `username` varchar(50) NOT NULL,
     `email` varchar(255) NOT NULL,
     `hash_password` longblob NOT NULL,
-    `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
