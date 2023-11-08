88ad318bbc2752ce328f2bccdfe4dcd37efa6ff9
bangthong <bangthong@teqnological.asia>
2023-11-08 17:11:50 +0700

    fix: parse uuid logic

--- usecase/product/update.go
+++ usecase/product/update.go
@@ -26,8 +26,13 @@ func (u *UseCase) Update(ctx context.Context, req *payload.UpdateProductRequest)
 			return nil, myerror.ErrProductInvalidParam(err.Error())
 		}
 	}
+	productID, err := uuid.Parse(req.ID)
+	if err != nil {
+		return nil, myerror.ErrProductInvalidParam(err.Error())
+	}
+
 	product := &model.Product{
-		ID:          uuid.MustParse(req.ID),
+		ID:          productID,
 		Name:        req.Name,
 		ProductType: req.ProductType,
 		ProducerID:  producer,
